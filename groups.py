d07c132b2b6dec74d888b047149ce3f0bdb07cfb
Кагадий Андрей <kagadi.a@gmail.com>
2024-02-07 15:51:03 +0300

    modified:   main.py

--- groups.py
+++ groups.py
@@ -11,19 +11,24 @@ config = configparser.ConfigParser()
 config.read("settings.ini")
 
 TIME_DELTA = int(config.get("DEFAULT", "time_delta"))
-QUANTITY_SOL = int(config.get("DEFAULT", "quantity_sol"))
-LP_FLAG = config.get("DEFAULT", "lp")
-MINT_FLAG = config.get("DEFAULT", "mint")
-FREEZE_FLAG = config.get("DEFAULT", "freeze")
+QUANTITY_SOL = float(config.get("DEFAULT", "quantity_sol"))
+QUANTITY_DEALS = int(config.get("DEFAULT", "quantity_deals"))
+LP_FLAG = config.get("DEFAULT", "lp").lower()
+MINT_FLAG = config.get("DEFAULT", "mint").lower()
+FREEZE_FLAG = config.get("DEFAULT", "freeze").lower()
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
+# API_ID = os.getenv("API_ID")
+# API_HASH = os.getenv("API_HASH")
+# TARGET_USER = "JLeagua_bot"
+# TARGET_GROUP = "FreshWallets_Tracker_SOL"
 
-TARGET_USER = "JLeagua_bot"
-TARGET_GROUP = "FreshWallets_Tracker_SOL"
+API_ID = config.get("USER", "api_id")
+API_HASH = config.get("USER", "api_hash")
+TARGET_USER = config.get("DIRECTION", "target_user")
+TARGET_GROUP = config.get("FROM", "target_chat")
 
 REGEX_DICT = {
-    "quantity_sol": r"(?<=swapped )(?P<sol>.*)(?= SOL)",
+    "quantity_sol": r"(?<=swapped )(?P<sol>.*?)(?= SOL)",
     "token_address": r"(?<=Token Address: )(?P<tok_adr>.*)(?=\n)",
     "lp_flag": r"(?<=LP: .)(?P<lp_flag>\w*)(?=.\n)",
     "mint_flag": r"(?<=Mint: .)(?P<mint_flag>\w*)(?=.\n)",
@@ -33,56 +38,72 @@ REGEX_DICT = {
 data = {}
 client = TelegramClient("Kagadi_a", API_ID, API_HASH, system_version="4.16.30-vxCUSTOM")
 
-print(int(time.time()))
-current_time = time.time()
-formatted_time = time.ctime(current_time)
-print(formatted_time)
-
-# client.start()
-# for dialog in client.iter_dialogs():
-#     print(dialog.title)
-
 
 async def parse_message(message):
     message_data = []
     for values in REGEX_DICT.values():
-        message_data.append(re.search(values, message)[0])
+        element = re.search(values, message)
+        if element:
+            message_data.append(element[0])
+        else:
+            message_data.append(element)
     return message_data
+
+
+async def check_data(data):
+    check_series = {
+        "time": int(time.time()) - data["timestamp"] <= TIME_DELTA,
+        "sol": QUANTITY_SOL <= data["count_sol"],
+        "lp": LP_FLAG == data["lp_flag"],
+        "mint": MINT_FLAG == data["mint_flag"],
+        "freeze": FREEZE_FLAG == data["freeze_flag"],
+        "deals": QUANTITY_DEALS <= data["count_deals"],
+    }
+    return all(check_series.values())
+
+
+async def update_data(new_data):
+    token = new_data[1]
+    data[token]["count_sol"] = data[token]["count_sol"] + float(new_data[0])
+    data[token]["count_deals"] += 1
+
+
+async def clean_data():
+    if data:
+        for key in list(data):
+            if int(time.time()) - data[key]["timestamp"] >= TIME_DELTA:
+                del data[key]
 
 
 @client.on(events.NewMessage(chats=TARGET_GROUP))
 async def normal_handler(event):
+    await clean_data()
     message = await parse_message(event.message.to_dict()["message"])
     if all(message):
+        token = message[1]
+
+        if token not in data:
+            meta_data = {
+                "timestamp": int(time.time()),
+                "count_sol": float(message[0]),
+                "lp_flag": message[2].lower(),
+                "mint_flag": message[3].lower(),
+                "freeze_flag": message[4].lower(),
+                "count_deals": 1,
+            }
+            data[token] = meta_data
+        else:
+            await update_data(message)
+
+        if await check_data(data[token]):
+            await client.send_message(TARGET_USER, ", ".join(message))
+    else:
         print(message)
-        timestamp = int(time.time())
-        meta_data = {
-            "timestamp": timestamp,
-            "count_sol": message[0],
-            "lp_flag": message[2],
-            "mint_flag": message[3],
-            "freeze_flag": message[4],
-        }
-        data.setdefault(message[1], meta_data)
-    print(data)
-    # if result:
-    #     print(result[0])
-
-    await client.send_message(TARGET_USER, " ".join(message))
-
-    # s_user_id = event.message.to_dict()["from_id"]
-    # user_id = int(s_user_id)
-    # user = d.get(user_id)
-
-    # mess_date = event.message.to_dict()["date"]
-
-    # f.write(mess_date.strftime("%d-%m-%Y %H:%M") + "\n")
-    # f.write(user + "\n")
-    # f.write(user_mess + "\n\n")
-
-    # f.flush()
+    for key in data.keys():
+        print(f"{key} - {data[key]}")
+    print("\n--------------------------------\n")
 
 
-client.start()
-
-client.run_until_disconnected()
+if __name__ == "__main__":
+    client.start()
+    client.run_until_disconnected()
